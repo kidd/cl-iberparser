e0132cfc85cb59ce0668613358e92e6edfafa9b9
Raimon Grau <raimonster@gmail.com>
2017-10-16 13:26:12 +0100

    Reordered code

--- src/cl-iberparser.lisp
+++ src/cl-iberparser.lisp
@@ -3,6 +3,29 @@
   (:use :cl))
 (in-package :cl-iberparser)
 
+
+(defun parse-euros (txt)
+  (handler-case
+      (parse-float:parse-float
+       (ppcre:regex-replace "^.*EUR (.*) *$"
+                            (substitute #\Space #\NewLine (plump:text txt))
+                            "\\1")
+       :decimal-character #\,)
+    (error () 0)))
+
+(defun price-for-book (book-url)
+  (or
+   (loop
+     with web = (plump:parse (drakma:http-request book-url))
+     for price-txt across (clss:select
+                           "div.result-pricing > div.item-price span.price" web)
+     for price = (parse-euros price-txt)
+     for shipping-txt across (clss:select "div.result-pricing div.shipping" web)
+     for shipping = (parse-euros shipping-txt)
+     collect (+ price shipping))
+   (list 100000)) ;; Superhack.works for now
+  )
+
 (defun prices-for-lines-in (file)
   (let ((content-lines (ppcre::split "\\n"
                                      (alexandria:read-file-into-string file))))
@@ -11,30 +34,3 @@
           when (<= (first (price-for-book book-url))
                    (parse-float:parse-float (or max-price 100000)))
             collect book-url)))
-
-(defun price-for-book (book-url)
-  (or
-   (loop
-     with web = (plump:parse (drakma:http-request book-url))
-     for price-txt across (clss:select "div.result-pricing > div.item-price span.price"
-                                       web)
-
-     for price = (parse-float:parse-float (ppcre:regex-replace
-                                           "EUR (.*)"
-                                           (plump:text price-txt)
-                                           "\\1")
-                                          :decimal-character #\,)
-     for shipping-txt across (clss:select "div.result-pricing div.shipping" web)
-     for shipping = (handler-case
-                        (parse-float:parse-float
-                         (ppcre:regex-replace
-                          "^.*EUR (.*) *$"
-                          (substitute #\Space #\NewLine (plump:text shipping-txt))
-                          "\\1")
-                         :decimal-character #\,)
-                      (error ()
-                        0))
-     collect (+ price shipping))
-   (list 100000)) ;; Superhack.works for now
-  )
-;; blah blah blah.
